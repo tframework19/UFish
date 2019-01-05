8b8424bb38fe0619069af6b6cd89648c1355e94a
tframework19 <tframework@bk.ru>
2019-01-05 19:46:50 +0300

    MobAreaSl4
    +4.46 ELO (green)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -583,7 +583,7 @@ namespace {
     if (pos.count<QUEEN>(Them) == 1)
     {
         Square s = pos.square<QUEEN>(Them);
-        safe = mobilityArea[Us] & ~stronglyProtected;
+        safe = (mobilityArea[Us] | pos.pieces(Us, QUEEN)) & ~stronglyProtected;
 
         b = attackedBy[Us][KNIGHT] & pos.attacks_from<KNIGHT>(s);
 
