fa3d128b5b8b385c3807f2dba0df79a9b4fc3ba0
tframework19 <tframework@bk.ru>
2019-01-05 22:48:50 +0300

    backward (increase mg, decrease eg, +/-5. bench 3677424)
    +4.22 ELO (green)

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,7 +32,7 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
-  constexpr Score Backward = S( 9, 24);
+  constexpr Score Backward = S(14, 19);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
 
