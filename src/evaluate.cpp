a8bb375cf54447af4a09373d73d981254945348f
tframework19 <tframework@bk.ru>
2019-01-05 19:49:12 +0300

    QoverloadImb8
    +2.94 ELO (green)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -593,6 +593,11 @@ namespace {
            | (attackedBy[Us][ROOK  ] & pos.attacks_from<ROOK  >(s));
 
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
+		 if (pos.count<QUEEN>(Us) == 0)
+            {
+            b = attackedBy[Us][ALL_PIECES] & attackedBy[Them][QUEEN] & ~attackedBy2[Them] & pos.pieces(Them);
+            score += make_score(20, 20) * more_than_one(b);
+            }
     }
 
     if (T)
