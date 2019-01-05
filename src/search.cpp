aef367e6123ef2db2d2f6be2315b8493c8a7333e
tframework19 <tframework@bk.ru>
2019-01-05 19:37:52 +0300

    countTw6
    +2.59 ELO (green)

--- src/search.cpp
+++ src/search.cpp
@@ -1201,7 +1201,7 @@ moves_loop: // When in check, search starts from here
 
     }
     // Bonus for prior countermove that caused the fail low
-    else if (   (depth >= 3 * ONE_PLY || PvNode)
+    else if (   (depth >= 3 * ONE_PLY || PvNode || ((ss-1)->moveCount == 1))
              && !pos.captured_piece())
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
 
